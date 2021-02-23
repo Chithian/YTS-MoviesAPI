e09c8759f3da250c9d6ea2e44b5f0e23ab1e6a07
Chithian(Hunter) <chithiansamony@gmail.com>
2021-02-23 12:55:02 +0700

    first commit

---
+++ YTS MoviesAPI/Model/MovieData.swift
@@ -0,0 +1,8 @@
+//
+//  MovieData.swift
+//  YTS MoviesAPI
+//
+//  Created by Chithian on 23/2/21.
+//
+
+import Foundation
---
+++ YTS MoviesAPI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  YTS MoviesAPI
+//
+//  Created by Chithian on 23/2/21.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+       
+    }
+
+
+}
+
